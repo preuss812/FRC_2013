7f1716dcdf249c0f9d6df74a5e5c88ca4d7ec015
Midnight Mechanics <preuss812-gitwin@northpb.com>
2013-02-12 15:01:34 -0800

    Refactor for production robot

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,4 +1,47 @@
 #include "WPILib.h"
+
+#define KP 5/10
+#define KI 0
+#define KD 0
+
+class PIDController812
+{
+	float prev_err, integral_err;
+public:
+	float PIDCalc(float, float);
+	void Reset(void);
+};
+
+float PIDController812::PIDCalc( float setPoint, float currentPoint) {
+	float error;
+	float delta_err;
+	float p_out;
+	float i_out;
+	float d_out;
+	float output;
+		
+	error = currentPoint - setPoint;
+	delta_err = prev_err - error;
+	integral_err += error;
+	
+	fprintf(stderr, "PID data: error %f, delta_err %f, integral_err %f\n",
+			error,
+			delta_err,
+			integral_err);
+	
+	p_out = error * KP;
+	i_out = integral_err * KI;
+	d_out = delta_err * KD;
+		
+	output = p_out + i_out + d_out;
+		
+	return (output);
+};
+
+void PIDController812::Reset() {
+	prev_err = 0.0;
+	integral_err = 0.0;
+};
 /**
  * This is a demo program showing the use of the RobotBase class.
  * The SimpleRobot class is the base of a robot application that will automatically call your
@@ -7,25 +50,31 @@
  */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
+//	RobotDrive myRobot; // robot drive system
 	DriverStation *m_ds;	// driver station object
 	Joystick leftStick; // left
 	Joystick rightStick; // right
 	Joystick climbStick;
-	Jaguar *encoderJaguar_1;
-	Jaguar *encoderJaguar_2;
+	Jaguar *Motor1;	// left wheel
+	Jaguar *Motor2;	// right wheel
+	Jaguar *Motor3;
+	Jaguar *Motor4;
+	Jaguar *Motor5;
+	Jaguar *Motor6;
+	Jaguar *Motor7;
+	RobotDrive *myRobot;
 	Relay spike;
 	Counter ferroDetector;
 	Encoder encoder_1;
 	Encoder encoder_2;
-	PIDController *pid_controller_2;
+	PIDController812 *pid_controller_2;
 	
 public:
 	RobotDemo(void):
 	//	RobotDrive(UINT32 frontLeftMotorChannel, UINT32 rearLeftMotorChannel,
 	//					UINT32 frontRightMotorChannel, UINT32 rearRightMotorChannel);
 		//myRobot(3,4,2,1),
-		myRobot(1,2),
+//		myRobot(1,2),
 		leftStick(1),		// as they are declared above.
 		rightStick(2), // right stick
 		climbStick(3),
@@ -34,16 +83,18 @@ public:
 		encoder_1(5,6),   // digital I/O port 5 == chan A, port 6 == chan B
 		encoder_2(8,9)	// digital I/O port 8 == chan A, port 9 == chan B
 	{
-		myRobot.SetExpiration(0.1);
-		encoderJaguar_1 = new Jaguar(3); // PWM 3
-		encoderJaguar_2 = new Jaguar(4); // PWM 4
-		float p = 0;
-		float i = 0;
-		float d = 0;
-		PIDSource *source = /*dynamic_cast<PIDSource *>*/ &encoder_1;
-		PIDOutput *output = /*dynamic_cast<PIDOutput *>*/ encoderJaguar_2;
-		pid_controller_2 = new PIDController(p, i, d, source, output);
-		pid_controller_2->SetSetpoint(0);
+//		myRobot.SetExpiration(0.1);  
+		Motor1 = new Jaguar(1);
+		Motor2 = new Jaguar(2);
+		Motor3 = new Jaguar(3); // PWM 3
+		Motor4 = new Jaguar(4); // PWM 4
+		Motor5 = new Jaguar(5);
+		Motor6 = new Jaguar(6);
+		Motor7 = new Jaguar(7);
+
+		pid_controller_2 = new PIDController812;
+
+		myRobot = new RobotDrive(Motor1, Motor2);
 	}
 
 	/**
@@ -51,38 +102,33 @@ public:
 	 */
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
+	//	myRobot.SetSafetyEnabled(false);
 		// Distance = velocity / time; 
 		// Assume instaneous acceleration to make things easy
-		float distance = 20.0; // feet
+		float distance = 40.0; // feet
 		float velocity = 10.0; // feet / sec
 		float drive_time = distance / velocity; // t (sec) = d (feet) / v (feet/sec)
 		
-		SmartDashboard::PutNumber("autonomous: drive_time", drive_time);
+		fprintf(stderr,"autonomous: drive_time %f\n", drive_time);
+		Motor1->SetSpeed(0.5);
+		Motor2->SetSpeed(-0.5);
+		
+		Wait(drive_time);
+		
+		Motor1->SetSpeed(0.0);
+		Motor2->SetSpeed(0.0);
 
 		// Default robot drive code w/single speed and curve parameters, no real
 		// ability to adjust wheel speed based on encoder feedback other than
 		// recalculate a curvature value.
 		// Assuming we run the robot at full speed
-		for (float curve = -1.0; curve<=1.0  ; curve += 0.1 ) {
+/*		for (float curve = -1.0; curve<=1.0  ; curve += 0.1 ) {
 			SmartDashboard::PutNumber("autonomous: curve", (double)curve);
-			myRobot.Drive(1.0, curve);
+						myRobot.Drive(1.0, curve);
 			Wait(drive_time);
 			myRobot.Drive(0.0, 0.0); 	// stop robot
-		}
-
-
-#ifdef dans_bad_code
-		// Individual motor control
-		Jaguar *left_motor = new Jaguar(1);
-		Jaguar *right_motor = new Jaguar(2);
-		
-		left_motor->Set(1.0);
-		right_motor->Set(0.5);
-		Wait(drive_time);
-		left_motor->Set(0.0);
-		right_motor->iSet(0.0);
-#endif
+	}
+*/
 
 		/*
 		myRobot.Drive(0.25, 0.0); 	// drive forwards half speed
@@ -100,42 +146,43 @@ public:
 	void OperatorControl(void)
 	{
 		bool windowMotorOn = FALSE;
-		float encoderMotorSpeed = 0.0;
+		float jaguarSpeed_1 = 0.0;
+		float jaguarSpeed_2 = 0.0;
+		float jagAdjust;
 		double sumEncoderRates_1;
 		double sumEncoderRates_2;
 		int countRates = 0;
 		const int periods = 100;
+		float adjustment;
 		
-		myRobot.SetSafetyEnabled(true);
-		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true); //right
-		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true); //left
+//		myRobot.SetSafetyEnabled(true);
+//		myRobot.SetInvertedMotor(RobotDrive::kRearLeftMotor, true); //right
+//		myRobot.SetInvertedMotor(RobotDrive::kRearRightMotor, true); //left
 		ferroDetector.Start();
 		encoder_1.Start();
 		encoder_2.Start();
-//		newEncoder.SetDistancePerPulse(6*3.14/4096.0);
 		encoder_1.SetDistancePerPulse(360.0/4096.0);
 		encoder_2.SetDistancePerPulse(360.0/4096.0);
-
-//		newEncoder.SetMinRate(0.1);
 		
 		pid_controller_2->Reset();
-		pid_controller_2->Enable();
+//		pid_controller_2->Enable();
 		
 		while (IsOperatorControl())
 		{
 			//myRobot.ArcadeDrive(rightStick); // drive with arcade style (use right stick)
-		    myRobot.TankDrive(leftStick, rightStick);
+//		    myRobot.TankDrive(leftStick, rightStick);
 			Wait(0.005);				// wait for a motor update time
 			if(rightStick.GetRawButton(1))
 			{
-				encoderMotorSpeed += 0.05;
-				if( encoderMotorSpeed > 0.5) {
-					encoderMotorSpeed = 0.0;
+				jaguarSpeed_1 += 0.05;
+				if( jaguarSpeed_1 > 0.5) {
+					jaguarSpeed_1 = 0.0;
 				}
-				encoderJaguar_1->SetSpeed(encoderMotorSpeed);
-				//encoderJaguar_2->SetSpeed(encoderMotorSpeed);
-				pid_controller_2->SetSetpoint(encoder_1.GetRate());
-				fprintf(stderr,"OnTarget? %d", pid_controller_2->OnTarget());
+				jaguarSpeed_2 = jaguarSpeed_1;
+				Motor3->SetSpeed(jaguarSpeed_1);
+				Motor4->SetSpeed(jaguarSpeed_2);
+//				pid_controller_2->SetSetpoint(encoder_1.GetRate());
+//				fprintf(stderr,"OnTarget? %d\n", pid_controller_2->OnTarget());
 			}
 			if(leftStick.GetRawButton(1) )
 			{
@@ -156,21 +203,38 @@ public:
 			sumEncoderRates_1 += encoder_1.GetRate();
 			sumEncoderRates_2 += encoder_2.GetRate();
 			countRates += 1;
+
 			if( countRates >= periods ) {
-				fprintf(stderr,"motor: %f, enc_1 avg_1: %f, enc_1 deg: %f\n",
-						encoderMotorSpeed, 
+				fprintf(stderr,"motor_1: %f, enc_1 avg_1: %f, enc_1 deg: %f\n",
+						jaguarSpeed_1, 
 						sumEncoderRates_1/periods, 
 						sumEncoderRates_1/periods/360.0*60.0);
-				fprintf(stderr,"motor: %f, enc_2 avg_2: %f, enc_2 deg: %f\n", 
-						encoderMotorSpeed, 
+				fprintf(stderr,"motor_2: %f, enc_2 avg_2: %f, enc_2 deg: %f\n", 
+						jaguarSpeed_2, 
 						sumEncoderRates_2/periods, 
 						sumEncoderRates_2/periods/360.0*60.0);
 				fprintf(stderr,"deltas: avg: %f, deg %f\n",
 						sumEncoderRates_1/periods - sumEncoderRates_2/periods,
 						(sumEncoderRates_2/periods/360.0*60.0) - (sumEncoderRates_1/periods/360.0*60.0)	);
 
-						
-						
+				
+				adjustment = pid_controller_2->PIDCalc(
+						encoder_1.GetRate(),
+						encoder_2.GetRate());
+				jagAdjust = adjustment*0.2/700.0;
+
+				if( jagAdjust*jagAdjust > 0.05*0.05) {
+					Motor4->SetSpeed(Motor4->Get() + jagAdjust);
+					jaguarSpeed_2 = Motor4->Get();
+					fprintf(stderr,"Jaguar adjustment: %f\n", jagAdjust);
+				}
+				
+				if((jaguarSpeed_2 - jaguarSpeed_1) >= 0.2)
+				{
+					jaguarSpeed_2 = jaguarSpeed_1;
+					Motor4->SetSpeed(jaguarSpeed_2);
+					fprintf(stderr,"RESET: \n");
+				}
 				countRates = 0;
 				sumEncoderRates_1 = 0;
 				sumEncoderRates_2 = 0;
