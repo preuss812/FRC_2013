7dd1aef5ab9edce53917182cd147cf05be6dfa3e
Midnight Mechanics <preuss812-gitwin@northpb.com>
2013-02-16 17:13:30 -0800

    Climbing arm feedback/adjustment code implemented, tests well.
    Logic has been added to handle the potential states of the climbing
    motors.

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,4 +1,5 @@
 #include "WPILib.h"
+#include <math.h>
 
 #define KP 5/10
 #define KI 0
@@ -60,8 +61,8 @@ class RobotDemo : public SimpleRobot
 	Jaguar *Motor2;	// right wheel
 	Jaguar *Motor3;
 	Jaguar *Motor4;
-	Jaguar *Motor5;
-	Jaguar *Motor6;
+	Jaguar *Motor5RightTower;
+	Jaguar *Motor6LeftTower;
 	Jaguar *Motor7;
 	RobotDrive *myRobot;
 //	Relay spike;
@@ -69,8 +70,8 @@ class RobotDemo : public SimpleRobot
 	Encoder encoder_2;
 	PIDController812 *pid_controller_1;
 	PIDController812 *pid_controller_2;
-	Counter Counter1;
-	Counter Counter2;
+	Counter Counter6LeftTower;
+	Counter Counter7RightTower;
 //	Counter Counter3;
 	
 public:
@@ -80,16 +81,16 @@ public:
 		climbStick(3),
 		encoder_1(1,2), // digital I/O port 1 == chan A, port 2 == chan B
 		encoder_2(3,4),	// digital I/O port 3 == chan A, port 4 == chan B
-		Counter1(6),	// dio port 6 counter on left tower motor
-		Counter2(7)		// dio port 7 counter on right tower motor
+		Counter6LeftTower(6),	// dio port 6 counter on left tower motor
+		Counter7RightTower(7)		// dio port 7 counter on right tower motor
 	{
 		Motor1 = new Jaguar(1);
 		Motor2 = new Jaguar(2);
 		Motor3 = new Jaguar(3); // PWM 3
 		Motor4 = new Jaguar(4); // PWM 4
-		Motor5 = new Jaguar(5);
-		Motor6 = new Jaguar(6);
-		Motor7 = new Jaguar(7);
+		Motor5RightTower = new Jaguar(5);
+		Motor6LeftTower = new Jaguar(6); // left tower jaguar
+		Motor7 = new Jaguar(7); // right tower jaguar
 
 		pid_controller_1 = new PIDController812;
 		pid_controller_2 = new PIDController812;
@@ -195,19 +196,23 @@ public:
 		int countRates = 0;
 		const int periods = 100;
 		float adjustment;
-		float liftSpeed5 = 0.2;
-		float liftSpeed6 = 0.45;
-		int CounterPrev1, CounterPrev2;
+		float plungerSpeed = 0.50;
+		float liftSpeed5Right = 0.80;
+		float liftSpeed6Left = 0.80;
+		float liftPercentOfFull = 0.30;
+		int CounterCur6LeftTower, CounterCur5RightTower;
+		int CounterPrev6LeftTower, CounterPrev5RightTower;
 		float MotorSpeed5, MotorSpeed6;
 		float liftAdjust;
-		int liftAdjustCycle = 100;
+		float liftCounterRatio;
+		enum {DOWN, STOP, UP} liftStatus;
 		
 		myRobot->SetSafetyEnabled(true);
 		myRobot->SetInvertedMotor(RobotDrive::kRearLeftMotor, true); //right
 		myRobot->SetInvertedMotor(RobotDrive::kRearRightMotor, true); //left
 
-		Counter1.Start();
-		Counter2.Start();
+		Counter6LeftTower.Start();
+		Counter7RightTower.Start();
 
 		
 		encoder_1.Start();
@@ -218,6 +223,8 @@ public:
 		pid_controller_1->Reset();
 		pid_controller_2->Reset();
 		
+		liftStatus = STOP;
+		
 		while (IsOperatorControl())
 		{
 			//myRobot.ArcadeDrive(rightStick); // drive with arcade style (use right stick)
@@ -226,134 +233,93 @@ public:
 			
 			if(rightStick.GetRawButton(1))
 			{
-				Motor7->SetSpeed(0.9);
+				Motor7->SetSpeed(plungerSpeed);
 			}
 			if(rightStick.GetRawButton(2))
 			{
-				Motor7->SetSpeed(-0.9);
+				Motor7->SetSpeed(-plungerSpeed);
 			}
 			if(rightStick.GetRawButton(3))
 			{
-				Motor5->SetSpeed(0.0);
-				Motor6->SetSpeed(0.0);
+				Motor5RightTower->SetSpeed(0.0);
+				Motor6LeftTower->SetSpeed(0.0);
 				Motor7->SetSpeed(0.0);
-				Wait(0.2);
+				Wait(0.3);
 
-				Counter1.Reset();
-				Counter2.Reset();
+				Counter6LeftTower.Reset();
+				Counter7RightTower.Reset();
+				
+				liftStatus = STOP;
 			}
 			
 			if(rightStick.GetRawButton(4))
 			{
-				Motor5->SetSpeed(liftSpeed5);
-				Motor6->SetSpeed(liftSpeed6);
-				Counter1.Reset();
-				Counter2.Reset();
+				Motor5RightTower->SetSpeed(liftSpeed5Right);
+				Motor6LeftTower->SetSpeed(liftSpeed6Left);
+			//	Counter6LeftTower.Reset();
+			//	Counter7RightTower.Reset();
 				pid_controller_1->Reset();
+				liftStatus = DOWN;
 			}
 			if(rightStick.GetRawButton(5))
 			{
-				Motor5->SetSpeed(-liftSpeed5);
-				Motor6->SetSpeed(-liftSpeed6);
-				Counter1.Reset();
-				Counter2.Reset();
+				Motor5RightTower->SetSpeed(-liftSpeed5Right);
+				Motor6LeftTower->SetSpeed(-liftSpeed6Left);
+			//	Counter6LeftTower.Reset();
+			//	Counter7RightTower.Reset();
 				pid_controller_1->Reset();
+				liftStatus = UP;
 			}
 			
-/* 2012-02-12
- * This code section was used for PID controller testing. It is disabled
- * in favor of TankDrive listed above
- * 
-			if(rightStick.GetRawButton(1))
+			CounterCur6LeftTower = Counter6LeftTower.Get();
+			CounterCur5RightTower = Counter7RightTower.Get();
+		
+			if( CounterCur6LeftTower != CounterPrev6LeftTower || CounterCur5RightTower != CounterPrev5RightTower)
 			{
-				jaguarSpeed_1 += 0.05;
-				if( jaguarSpeed_1 > 0.5) {
-					jaguarSpeed_1 = 0.0;
-				}
-				jaguarSpeed_2 = jaguarSpeed_1;
-				Motor3->SetSpeed(jaguarSpeed_1);
-				Motor4->SetSpeed(jaguarSpeed_2);
-//				pid_controller_2->SetSetpoint(encoder_1.GetRate());
-//				fprintf(stderr,"OnTarget? %d\n", pid_controller_2->OnTarget());
-			}
---- end of PID controller testing code 
-*/
-			if( Counter1.Get() != CounterPrev1 || Counter2.Get() != CounterPrev2)
-			{
-				CounterPrev1 = Counter1.Get();
-				CounterPrev2 = Counter2.Get();
-				fprintf(stderr,"lift counter1 count: %d\n", CounterPrev1);
-				fprintf(stderr,"lift counter2 count: %d\n", CounterPrev2);
-				liftAdjust=pid_controller_1->PIDCalc(Counter2.Get(),Counter1.Get());
-				fprintf(stderr,"liftAdjust: %f\n", liftAdjust);
-				MotorSpeed6 = Motor6->Get();
-				MotorSpeed5 = Motor5->Get();
-				fprintf(stderr,"motor5 speed: %f, motor6 speed: %f\n",MotorSpeed5, MotorSpeed6);
-				Motor5->SetSpeed(MotorSpeed5 + MotorSpeed5*liftAdjust/100.0);
-				fprintf(stderr,"motor5 speed: %f, motor6 speed: %f\n",
-						Motor5->Get(),
-						Motor6->Get());
-			}	
-/*
-			if(leftStick.GetRawButton(1) )
-			{
-				spike.Set(Relay::kForward);
-				windowMotorOn = TRUE;
-			}
-			if(leftStick.GetRawButton(2) )
-			{
-				spike.Set(Relay::kOff);
-				windowMotorOn = FALSE;
-			}
-			if(leftStick.GetRawButton(3) )
-			{
-				spike.Set(Relay::kReverse);
-				windowMotorOn = TRUE;
-			}
-*/			
-			sumEncoderRates_1 += encoder_1.GetRate();
-			sumEncoderRates_2 += encoder_2.GetRate();
-			countRates += 1;
-
-/*			if( countRates >= periods ) {
-				fprintf(stderr,"motor_1: %f, enc_1 avg_1: %f, enc_1 deg: %f\n",
-						jaguarSpeed_1, 
-						sumEncoderRates_1/periods, 
-						sumEncoderRates_1/periods/360.0*60.0);
-				fprintf(stderr,"motor_2: %f, enc_2 avg_2: %f, enc_2 deg: %f\n", 
-						jaguarSpeed_2, 
-						sumEncoderRates_2/periods, 
-						sumEncoderRates_2/periods/360.0*60.0);
-				fprintf(stderr,"deltas: avg: %f, deg %f\n",
-						sumEncoderRates_1/periods - sumEncoderRates_2/periods,
-						(sumEncoderRates_2/periods/360.0*60.0) - (sumEncoderRates_1/periods/360.0*60.0)	);
-
-				
-				adjustment = pid_controller_2->PIDCalc(
-						encoder_1.GetRate(),
-						encoder_2.GetRate());
-				jagAdjust = adjustment*0.2/700.0;
-
-				if( jagAdjust*jagAdjust > 0.05*0.05) {
-			 		Motor4->SetSpeed(Motor4->Get() + jagAdjust);
-					jaguarSpeed_2 = Motor4->Get();
-					fprintf(stderr,"Jaguar adjustment: %f\n", jagAdjust);
-				}
-				
-				if((jaguarSpeed_2 - jaguarSpeed_1) >= 0.2)
+				CounterPrev6LeftTower = CounterCur6LeftTower;
+				CounterPrev5RightTower = CounterCur5RightTower;
+				fprintf(stderr, "LeftTower speed %f, rightTower speed %f\n",
+						Motor6LeftTower->Get(),
+						Motor5RightTower->Get());
+				fprintf(stderr,"LeftTower count %d, rightTower count %d\n",
+						CounterCur6LeftTower, CounterCur5RightTower);
+					
+				if( fabs( (double)CounterCur6LeftTower - (double) CounterCur5RightTower) >= 0.1)
 				{
-					jaguarSpeed_2 = jaguarSpeed_1;
-					Motor4->SetSpeed(jaguarSpeed_2);
-					fprintf(stderr,"RESET: \n");
+					if(CounterCur6LeftTower > CounterCur5RightTower && liftStatus == UP)
+					{
+						fprintf(stderr, "Turning off LeftTower because Left is UP ahead of Right.\n");
+						Motor6LeftTower->SetSpeed(-liftSpeed6Left*liftPercentOfFull);
+						Motor5RightTower->SetSpeed(-liftSpeed5Right);
+					} else if (CounterCur5RightTower > CounterCur6LeftTower && liftStatus == UP)
+					{
+						fprintf(stderr, "Turning off RightTower because Right is UP ahead of Left.\n");
+						Motor6LeftTower->SetSpeed(-liftSpeed6Left);
+						Motor5RightTower->SetSpeed(-liftSpeed5Right*liftPercentOfFull);
+					} else if (CounterCur6LeftTower > CounterCur5RightTower && liftStatus == DOWN)
+					{
+						fprintf(stderr, "Turning off LeftTower because Left is DOWN ahead of Right.\n");
+						Motor6LeftTower->SetSpeed(liftSpeed6Left*liftPercentOfFull);
+						Motor5RightTower->SetSpeed(liftSpeed5Right);
+					} else if (CounterCur5RightTower > CounterCur6LeftTower && liftStatus == DOWN)
+					{
+						fprintf(stderr, "Turning off RightTower because Right is DOWN ahead of Left.\n");
+						Motor6LeftTower->SetSpeed(liftSpeed6Left);
+						Motor5RightTower->SetSpeed(liftSpeed5Right*liftPercentOfFull);
+					}
+				} else {
+					if( liftStatus == UP)
+					{
+						Motor6LeftTower->SetSpeed(-liftSpeed6Left);
+						Motor5RightTower->SetSpeed(-liftSpeed5Right);
+					} else if( liftStatus == DOWN)
+					{
+						Motor6LeftTower->SetSpeed(liftSpeed6Left);
+						Motor5RightTower->SetSpeed(liftSpeed5Right);
+					}
 				}
-				countRates = 0;
-				sumEncoderRates_1 = 0;
-				sumEncoderRates_2 = 0;
-				encoder_1.Reset();
-				encoder_2.Reset();
 			}
-			*/
-			}
+		}
 	}
 	
 	/**
